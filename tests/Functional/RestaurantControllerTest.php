fa10295194978b86019324825e35b6406f5b584d
packagespace <packagespace@pm.me>
2023-02-14 10:21:35 +0000

    Add header to base template

--- tests/Functional/RestaurantControllerTest.php
+++ tests/Functional/RestaurantControllerTest.php
@@ -6,12 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RestaurantControllerTest extends WebTestCase
 {
-    public function testIndex(): void
+    public function testIndexRendersWithCorrectTitle(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertPageTitleSame('Le Quai Antique');
+    }
+
+    public function testIndexRendersWithHeader()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSelectorExists('header');
     }
 }
