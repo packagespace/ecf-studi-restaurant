ce8c135350ae46807e566b7b45be1ac1e5d2f194
packagespace <packagespace@pm.me>
2023-02-16 16:29:54 +0000

    Display menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\DishCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    public function index(DishCategoryRepository $dishCategoryRepository): Response
     {
+        $dishCategories = $dishCategoryRepository->findAll();
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            'dish_categories' => $dishCategories,
         ]);
     }
 }
--- tests/Functional/Controller/MenuControllerTest.php
+++ tests/Functional/Controller/MenuControllerTest.php
@@ -13,4 +13,24 @@ class MenuControllerTest extends WebTestCase
 
         $this->assertPageTitleContains('Le Quai Antique - Carte et Menus');
     }
+
+    public function testMenuRendersCategoryNamesInAList()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/menu');
+
+        $this->assertSelectorExists('ul#dish-categories');
+        $this->assertSelectorExists('li.dish-category');
+    }
+
+    public function testMenuRendersCategoryDishes()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/menu');
+
+        $this->assertSelectorExists('dl.category-dishes');
+        $this->assertSelectorExists('dt.category-dish-name');
+        $this->assertSelectorExists('dd.category-dish-price');
+        $this->assertSelectorExists('dd.category-dish-description');
+    }
 }
