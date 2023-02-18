463e7d3322d727940cacf392cd9f6e0141950421
packagespace <packagespace@pm.me>
2023-02-18 15:00:14 +0000

    Try to fix migratin

--- migrations/Version20230217231448.php
+++ migrations/Version20230217231448.php
@@ -20,6 +20,16 @@ final class Version20230217231448 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE reservation_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE reservation (
+    id integer NOT NULL,
+    number_of_guests integer NOT NULL,
+    allergies text,
+    date date NOT NULL,
+    "time" integer NOT NULL,
+    PRIMARY KEY(id))'
+);
+
     }
 
     public function down(Schema $schema): void
