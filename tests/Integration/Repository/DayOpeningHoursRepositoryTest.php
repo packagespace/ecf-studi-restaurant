141fc414949d80709dcd58ee871c6673258adf2b
packagespace <packagespace@pm.me>
2023-02-20 15:44:57 +0100

    Fix day of week number order

---
+++ tests/Integration/Repository/DayOpeningHoursRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Integration\Repository;
+
+use App\Repository\DayOpeningHoursRepository;
+use PHPUnit\Framework\TestCase;
+
+class DayOpeningHoursRepositoryTest extends TestCase
+{
+
+    public function testFindAllInOrder()
+    {
+
+    }
+}
